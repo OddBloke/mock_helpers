ea242b27b60a117b7f09919d88accbe28f96e2b1
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 17:07:06 +0000

    Version bump.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='mock_helpers',
-    version='0.1.0',
+    version='0.1.1',
     author='Daniel Watkins',
     author_email='daniel@daniel-watkins.co.uk',
     url='http://pypi.python.org/pypi/mock_helpers',
