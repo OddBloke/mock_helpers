3919f4cb516605c0196fb8608dc2c4d6449b1acd
Daniel Watkins <daniel@daniel-watkins.co.uk>
2011-12-20 16:20:00 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+
+setup(
+    name='mock_helpers',
+    version='0.1.0',
+    author='Daniel Watkins',
+    author_email='daniel@daniel-watkins.co.uk',
+    url='http://pypi.python.org/pypi/mock_helpers',
+    description='A number of helpers for use with python-mock.',
+    packages=['mock_helpers'],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Software Development :: Testing",
+    ],
+)
